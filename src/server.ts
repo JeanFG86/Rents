70d667ef46f7f4addb62b7f1297826802a3ce1d1
jeanfg86 <jeanfg86@gmail.com>
2022-04-27 15:38:06 -0300

    configure debug

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,11 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
+app.post('/curses', (req, res) => {
+  const { name } = req.body
+  return res.json({ name })
+})
+
 app.listen(3333, () => console.log('Server is running'))
