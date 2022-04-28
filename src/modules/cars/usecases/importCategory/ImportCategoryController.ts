f01c809920b8c2e4b8c17bd7457a97352ba4ea9d
jeanfg86 <jeanfg86@gmail.com>
2022-04-28 16:53:05 -0300

    add csv-parse

--- src/modules/cars/usecases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/usecases/importCategory/ImportCategoryController.ts
@@ -5,7 +5,9 @@ export class ImportCategoryController {
   constructor (private readonly importCategoryUseCase: ImportCategoryUseCase) {}
   handle (request: Request, response: Response): Response {
     const { file } = request
-    this.importCategoryUseCase.execute(file)
+    if (file !== undefined) {
+      this.importCategoryUseCase.execute(file)
+    }
 
     return response.send()
   }
