62c55d6b26fb77dcaba1e55a8f2343f6f7636b32
jeanfg86 <jeanfg86@gmail.com>
2022-04-28 17:24:46 -0300

    refactor import category

--- src/modules/cars/usecases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/usecases/importCategory/ImportCategoryUseCase.ts
@@ -31,6 +31,17 @@ export class ImportCategoryUseCase {
 
   async execute (file: Express.Multer.File): Promise<void> {
     const categories = await this.loadCategories(file)
-    console.log(categories)
+
+    categories.map(async (category) => {
+      const { name, description } = category
+      const existCategory = this.categoriesRepository.findByName(name)
+
+      if (existCategory === undefined) {
+        this.categoriesRepository.create({
+          name,
+          description
+        })
+      }
+    })
   }
 }
